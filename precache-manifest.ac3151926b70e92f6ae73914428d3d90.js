4f080a61f27dcc42e8ef79cf37e2936d7d75863a
Valindo Godinho <valindo.godinho@gmail.com>
2020-05-15 22:06:24 +0530

    Updates

---
+++ precache-manifest.ac3151926b70e92f6ae73914428d3d90.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0102b6472da781cbb1d9b6edbc33b206",
+    "url": "/the-curse-of-c/index.html"
+  },
+  {
+    "revision": "99f0899a34c9e8e9f6b2",
+    "url": "/the-curse-of-c/static/css/main.8609bb4a.chunk.css"
+  },
+  {
+    "revision": "62e620fa8d8b9c5e1082",
+    "url": "/the-curse-of-c/static/js/2.0c993bf6.chunk.js"
+  },
+  {
+    "revision": "908678959397f2e04ac5f8f7afda467f",
+    "url": "/the-curse-of-c/static/js/2.0c993bf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99f0899a34c9e8e9f6b2",
+    "url": "/the-curse-of-c/static/js/main.c753b590.chunk.js"
+  },
+  {
+    "revision": "a225554f8f8cc5f10281",
+    "url": "/the-curse-of-c/static/js/runtime-main.66ccb67b.js"
+  },
+  {
+    "revision": "393be7983f495f676cfe4ecedf9c0b2c",
+    "url": "/the-curse-of-c/static/media/DS_Logo_Yellow.393be798.png"
+  },
+  {
+    "revision": "33b9fc0183557e898512d4b431e91dda",
+    "url": "/the-curse-of-c/static/media/The_Curse_Of_C-19 .33b9fc01.pdf"
+  }
+]);
